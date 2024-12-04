e71cf1ee0f4ac331b96bf9daa795de2f8defa92b
Alex Yan <alexyan@buffalo.edu>
2024-12-03 21:56:26 -0500

    Change the model to be ran on apple silicon.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     # results = model.train(data='./smiski_all/data.yaml',epochs=400,degrees=90,flipud=0.05,project='train',name='smiski_all',scale=.7,save_period=25,lr0=0.001)
     
     # Best trained model so far 
-    model = YOLO(r'runs\detect\train65\weights\last.pt')
-    model.to('cuda')
+    model = YOLO(r'./runs/detect/train65/weights/last.pt')
+    model.to('mps')
     model.predict('./predict_images',save=True, imgsz=640,name='train')
 
 if __name__ == "__main__":
