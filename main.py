c666bd1437bf748bc591130cd341a076265001c4
Alex Yan <alexyan@buffalo.edu>
2024-12-04 15:53:10 -0500

    Changed save parameters and display

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def main():
     # results = model.train(data='./smiski_all/data.yaml',epochs=400,degrees=90,flipud=0.05,project='train',name='smiski_all',scale=.7,save_period=25,lr0=0.001)
     
     # Best trained model so far 
-    model = YOLO(r'./runs/detect/train/weights/last.pt')
+    model = YOLO(r'./runs/detect/train/weights/best.pt')
     model.to('mps')
-    model.predict('./predict_images',save=True, imgsz=640,name='train')
-
+    results = model.predict('./predict_images',save=False, imgsz=640,name='train')
+    for result in results[:5]:
+        result.show()
 if __name__ == "__main__":
     main()
